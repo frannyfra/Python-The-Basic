725cf69bba21ff85344012aab8d0c60fc9397dfc
frannyfra <francescadelaurentis1987@gmail.com>
2021-06-29 12:39:15 +0100

    Indentation in python

--- example.py
+++ example.py
@@ -1,1 +1,18 @@
-print("Hello wordl!!!")
+
+# This is our first Python Script;
+# This is a comment!
+
+print("Hello world!!!")
+
+# Indentation:
+x = 1
+if x > 0:
+    print("x is greater than zero")
+else:
+    print("x is less than zero")
+
+# Variables and Data Types:
+
+age = 18
+print(type(age))
+# Will print <class "int> - same as typeof in JS
